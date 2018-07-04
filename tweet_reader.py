f92dffabe48357d8f1e78491e6b50fc1fe756e2f
HanGuo97 <alex.guohan1106@outlook.com>
2018-07-04 15:11:16 -0400

    added Tweeter

--- tweet_reader.py
+++ tweet_reader.py
@@ -4,7 +4,36 @@ import tweepy
 from TFLibrary.utils import misc_utils
 
 
-class TwitterWrapper(object):
+class TwitterTweeter(object):
+    def __init__(self,
+                 consumer_key, consumer_secret,
+                 access_token, access_token_secret):
+
+        auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+        auth.set_access_token(access_token, access_token_secret)
+        api = tweepy.API(auth, wait_on_rate_limit=True)
+
+        self._api = api
+        self._consumer_key = consumer_key
+        self._consumer_secret = consumer_secret
+        self._access_token = access_token
+        self._access_token_secret = access_token_secret
+
+    def _retweet(self, tweet_ID):
+        self._api.retweet(tweet_ID)
+
+    def retweet(self, tweet_IDs):
+        for tweet_ID in tweet_IDs:
+
+            try:
+                self._retweet(tweet_ID)
+                time.sleep(5)
+
+            except tweepy.TweepError as e:
+                print(e)
+
+
+class TwitterReader(object):
 
     def __init__(self, logdir,
                  consumer_key, consumer_secret,
@@ -34,29 +63,24 @@ class TwitterWrapper(object):
         # load previously cached data
         if load_from_history and \
                 os.path.exists(self.home_timeline_history_logdir) and \
-                os.path.exists(self.friendship_history_logdir) and \
-                os.path.exists(self.user_history_logdir):
+                os.path.exists(self.friendship_history_logdir):
 
             self._tweet_collections = misc_utils.load_object(
                 self.home_timeline_history_logdir)
             self._friendship_collections = misc_utils.load_object(
                 self.friendship_history_logdir)
-            self._user_collections = misc_utils.load_object(
-                self.user_history_logdir)
 
             misc_utils.assert_all_same([
                 len(self._tweet_collections),
-                len(self._friendship_collections),
-                len(self._user_collections)])
+                len(self._friendship_collections)])
 
         else:
             self._tweet_collections = []
             self._friendship_collections = []
-            self._user_collections = []
 
     def reading_tweets(self, screen_name="AlexGuoHan"):
         for tweets in tweepy.Cursor(self._api.home_timeline,
-                                  screen_name=screen_name).pages():
+                                    screen_name=screen_name).pages():
             # get friendship
             friendships = self._get_friendships(tweets)
 
@@ -65,9 +89,9 @@ class TwitterWrapper(object):
             self._friendship_collections.extend(friendships)
 
             misc_utils.save_object(self._tweet_collections,
-                self.home_timeline_history_logdir)
+                                   self.home_timeline_history_logdir)
             misc_utils.save_object(self._friendship_collections,
-                self.friendship_history_logdir)
+                                   self.friendship_history_logdir)
 
             yield tweets, friendships
             if self._debug:
@@ -79,7 +103,7 @@ class TwitterWrapper(object):
         filtered_tweets = []
         for tweet, friendship in zip(tweets, friendships):
             _be_kept = [fn(tweet, friendship)
-                for fn in self._filter_fns]
+                        for fn in self._filter_fns]
             if all(_be_kept):
                 filtered_tweets.append(tweet)
 
@@ -94,7 +118,6 @@ class TwitterWrapper(object):
 
         return processed_tweets
 
-
     def _get_friendships(self, tweets):
         friendships = []
         for tweet in tweets:
@@ -104,13 +127,12 @@ class TwitterWrapper(object):
             friendships.append(friendship)
         return friendships
 
-        
-        
-            
-        
-    
-
-
+    @property
+    def num_history(self):
+        if len(self._tweet_collections) != len(self._friendship_collections):
+            raise ValueError(
+                "len(tweet_collections) != len(friendship_collections)")
+        return len(self._tweet_collections)
 
     @property
     def home_timeline_history_logdir(self):
@@ -119,7 +141,3 @@ class TwitterWrapper(object):
     @property
     def friendship_history_logdir(self):
         return os.path.join(self._logdir, "friendship.pkl")
-
-    @property
-    def user_history_logdir(self):
-        return os.path.join(self._logdir, "user.pkl")
